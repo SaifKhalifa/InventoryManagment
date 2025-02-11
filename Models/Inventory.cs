a0879df903600bd8ba199f56c10c0fa792513895
Saif Khalifa <63987779+SaifKhalifa@users.noreply.github.com>
2025-02-11 12:39:56 +0200

    Inventory class added, needs implemntation!

---
+++ Models/Inventory.cs
@@ -0,0 +1,28 @@
+﻿namespace Models;
+
+public class Inventory
+{
+    List<Product> products = new List<Product>();
+       
+    public void AddProduct()
+    {
+    }
+
+    public void PrintAllProducts()
+    {
+    }
+
+    public int EditProduct()
+    {
+    }
+
+    public bool DeleteProduct()
+    {
+    }
+
+    public int SearchProduct()
+    {
+        
+    }
+    
+}
