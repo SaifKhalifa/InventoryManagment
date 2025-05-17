37d4d291ec477500b7b04921ef3ddc685f16ae6b
Saif Khalifa <63987779+SaifKhalifa@users.noreply.github.com>
2025-05-17 16:39:08 +0300

    Make Product class public and added 'id' attribute to align with the database schema

--- Models/Product.cs
+++ Models/Product.cs
@@ -1,6 +1,7 @@
 namespace Models;
-class Product
+public class Product
 {
+    public int Id { get; set; }
     public string Name { get; set; }
     public int Quantity { get; set; }
     public float Price { get; set; }
