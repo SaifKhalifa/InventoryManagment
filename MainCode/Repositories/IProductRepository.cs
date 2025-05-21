a370ae214ee0a05041ed7f9f1b8769aa7a77e809
Saif Khalifa <63987779+SaifKhalifa@users.noreply.github.com>
2025-05-21 09:47:06 +0300

    Add IProductRepository interface;
    Created `IProductRepository.cs` to define an interface for product management, including methods for adding, retrieving, updating, and deleting products, as well as checking the database connection.
    
    Removed the entire implementation of `ProductRepository.cs`.

---
+++ MainCode/Repositories/IProductRepository.cs
@@ -0,0 +1,13 @@
+﻿using Models;
+
+namespace InventoryManagment.Repositories;
+
+public interface IProductRepository
+{
+    Task AddProductAsync(Product product);
+    Task<List<Product>> GetAllProductsAsync();
+    Task<Product?> GetProductByIdAsync(int id);
+    Task UpdateProductAsync(Product product);
+    Task DeleteProductAsync(int id);
+    bool CheckDBConnection();
+}
